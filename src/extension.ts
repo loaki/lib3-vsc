0f150b2fd9af22bac4e64facdb75f952597982a8
loaki <loaki.dev@gmail.com>
2023-05-26 00:27:13 +0200

    test_dialog

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,25 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('lib3.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from lib3!');
+		
+		
+		let options = {
+			prompt: "Label: ",
+			placeHolder: "(placeholder)"
+		};
+		
+		vscode.window.showInputBox(options).then(value => {
+			if (value === "salut") {
+				vscode.window.showInformationMessage("Hello World from lib3!");
+			}
+			if (value === "c'est bien le ts?") {
+				vscode.window.showInformationMessage("Non, c'est vraiment a chier");
+			}
+			// show the next dialog, etc.
+		});
+
+
+
 	});
 
 	context.subscriptions.push(disposable);
